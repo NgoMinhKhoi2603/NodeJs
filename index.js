80975b0d565c6fb99773a956e544049959df4ec8
NgoMinhKhoi2603 <109575681+NgoMinhKhoi2603@users.noreply.github.com>
2022-12-11 18:50:13 +0700

    up

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3100;
 
 app.listen(PORT,function () {
     console.log("Server is running...")
